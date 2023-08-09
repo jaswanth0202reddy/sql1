b8964ce12fe01fbdef0a2c5f93bece659372d309
jaswanth0202reddy <140830503+jaswanth0202reddy@users.noreply.github.com>
2023-08-09 20:06:24 +0530

    Add files via upload

---
+++ PYTHON DAY1.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+name = "Akbar"
+name = "jaswanth"
+print(name)
+
+
+# In[4]:
+
+
+hema_age = jaswanth_age = venky_age = akbar_age = 21
+print("hema age =", hema_age)
+print("jaswanth age =", jaswanth_age)
+print("venky age =", venky_age)
+print("akbar age =", akbar_age)
+
+
+# In[6]:
+
+
+age = 21
+print(age)
+type(age)
+
+
+# In[8]:
+
+
+price_of_vegrice = 45.5
+price_of_cooldrink = 20
+print(price_of_vegrice)
+print(price_of_cooldrink)
+type(price_of_cooldrink)
+
+
+# In[9]:
+
+
+#in implicit
+price_unit = 6.5
+num_of_unit = 5
+total_price = 0
+total_price = price_unit*num_of_unit
+print(total_price)
+type(total_price)
+
+
+# In[ ]:
+
+
+#explicit
+price = 604
+tax = "2.5"
+total_price = price + float(tax)
+print(total_price)
+
+
+# In[11]:
+
+
+#type casting
+unit_price = 62.4
+str(unit_price)
+pas,fail=1,0
+print(bool(pas))
+print(bool(fail))
+
+
+# In[12]:
+
+
+b="2.3"
+int(b)
+
+
+# In[13]:
+
+
+#print statments
+print("hi","how","are","you")
+
+
+# In[14]:
+
+
+print("myname"+"isjaswanth")
+
+
+# In[15]:
+
+
+print("jaswanth age is" + 21)
+
+
+# In[17]:
+
+
+print("jaswanth age is " + str(21))
+
+
+# # ARITHMETIC OPERTORS
+
+# In[18]:
+
+
+#ADDITION
+price = 40
+tax = 10
+total = price + tax
+print(total)
+
+
+# In[19]:
+
+
+#sub
+price = 40
+tax = 10
+total = price - tax
+print(total)
+
+
+# In[20]:
+
+
+#mul
+price = 40
+tax = 10
+total = price * tax
+print(total)
+
+
+# In[22]:
+
+
+a=" hi "
+n=5
+result=a*n
+print(result)
+
+
+# In[23]:
+
+
+side = 30
+print("area of square=",side**2)
+print("area of cube= ",side**3)
+
+
+# In[ ]:
+
+
+
+
